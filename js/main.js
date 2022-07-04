47ecffa39adbf0f578ca1f4bc624bccb5e910b3e
João Guilherme Paiva <38386145+jguilhermepaiva@users.noreply.github.com>
2022-07-03 23:57:23 -0300

    :tada: primeiro commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:4
+        },
+        1000:{
+            items:4
+        },
+        1200:{
+            items:5
+        },
+        1400:{
+            items:6
+        }
+    }
+})
